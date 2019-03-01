9b69aa287096601b03d8fa7136338b0c7ca52890
techyon7 <sparshtyagi21@gmail.com>
2019-03-01 16:08:03 +0100

    Add particles.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,11 +15,11 @@
 //= require_tree .
 
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-
+/*
 particlesJS.load('particles-js', 'particles.json', function() {
   console.log('particles.js loaded - callback');
 });
-
+*/
 
 /* Otherwise just put the config content (json): */
 
