f215a1a4352f379b436d8ddf9b481aed07e80229
Alexander Pieczka <lexp@Alexanders-MacBook-Air.local>
2019-03-07 10:00:17 +0100

    Particles.js working in full extent

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,3 @@
 
 import 'particles.js';
 
-
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
