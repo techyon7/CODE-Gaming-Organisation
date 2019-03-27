c199d5d4a2ae0e14e80979fbd631a78e74fc162f
Alexander Pieczka <alexanderpieczka@ewe.net>
2019-03-27 17:25:16 +0100

    User sessions added to tabel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190326131934) do
+ActiveRecord::Schema.define(version: 20190327160625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20190326131934) do
     t.string "last_name"
     t.string "gamertag"
     t.string "profile_picture"
+    t.string "provider"
+    t.string "uid"
+    t.string "token"
+    t.integer "expires_at"
+    t.boolean "expires"
+    t.string "refresh_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
