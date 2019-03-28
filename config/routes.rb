cc304d05cdb4859313aa17355e8a840ea774ab31
Alexander Pieczka <alexanderpieczka@ewe.net>
2019-03-28 13:19:27 +0100

    Migration for join table User_events

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/update'
+
+  get 'user/update'
+
   root to: 'pages#home'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
