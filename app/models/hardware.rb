cc3a148688a9623f6008753a0b930d6077da6e4f
techyon7 <sparshtyagi21@gmail.com>
2019-03-30 09:51:16 +0100

    Implemented hardware controller

--- app/models/hardware.rb
+++ app/models/hardware.rb
@@ -1,2 +1,6 @@
 class Hardware < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :purpose, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   get 'home/index'
+  resources :hardwares
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
