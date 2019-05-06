b9c975ead4ed1520ff0e2d56bd6a2babff36dfc2
techyon7 <sparshtyagi21@gmail.com>
2019-05-06 13:07:03 +0200

    Added Foreign Keys associations to harware

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,18 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :events, through: :user_events
-  has_many :hardwares
-  
+  has_many :hardware_owners, :class_name => 'Hardware', :foreign_key => 'owner_id'
+  has_many :hardware_renters, :class_name => 'Hardware', :foreign_key => 'renter_id'
+  def hardware
+     hardware_owners + hardware_renters
+  end
+
 
 
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
-    
+
 
     # The user is created, if they don't exist
      unless user
